0719356c88770075b7ea5fbcd691079b08c4d972
Hyrum Durfee <htdurfee@gmail.com>
2025-12-03 12:38:46 -0700

    delete(writings) old writing samples folder

--- web/app/stories/[storyId]/writings/[writingId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import WritingSampleDetailClientPage from "./client-page";
-
-export default async function WritingSampleDetailPage({ params }: { params: Promise<{ storyId: string, writingId: string }> }) {
-  return (
-    <WritingSampleDetailClientPage params={await params} />
-  );
-}
