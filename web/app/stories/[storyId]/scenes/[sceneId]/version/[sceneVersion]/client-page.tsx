4dc228f5cb40e3ca24fd217ecd8fdc0019f94554
Hyrum Durfee <htdurfee@gmail.com>
2026-01-01 15:59:56 -0700

    update(web) added r as regenerate keyboard shortcut

--- web/app/stories/[storyId]/scenes/[sceneId]/version/[sceneVersion]/client-page.tsx
+++ web/app/stories/[storyId]/scenes/[sceneId]/version/[sceneVersion]/client-page.tsx
@@ -45,6 +45,10 @@ export default function SceneDetailClientPage({
         e.preventDefault()
         router.push(`/stories/${params.storyId}/scenes/${params.sceneId}/version/${params.sceneVersion}/edit`)
       }
+      else if (e.key === "r") {
+        e.preventDefault()
+        router.push(`/stories/${params.storyId}/scenes/${params.sceneId}/version/${params.sceneVersion}/regenerate`)
+      }
     }
 
     window.addEventListener("keydown", handleKeyDown)
